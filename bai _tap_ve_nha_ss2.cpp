74da7f1e3c27520895de1f8a692accb7ffd1bd05
HaHuyenTrang <147359400+HaHuyenTrang@users.noreply.github.com>
2023-11-07 22:30:51 +0700

    Add files via upload
    nộp btvn ss2 HHT

---
+++ bai _tap_ve_nha_ss2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+	//khai bao va khoi tao cac bien canh hinh vuong,chieu dai,chieu rong hinh chu nhat, ban kinh duong tron
+	float square_edge, rectangular_length, rectangular_width, circle_radius;
+	float squareEdge = 'a';
+	float rectangularLength = 'b';
+	float rectangularWidth = 'c';
+	float circleRadius = 'r';
+	//khai bao pi
+	const float pi = 3.14;
+	//tinh chu vi, dien tich hinh tron 
+	float diameter_of_a_circle = 2*pi*'r'; 
+	float circle_area = pi*'r'*'r'; 
+	//tinh chu vi, dien tich hinh chu nhat 
+	float  perimeter_of_a_rectangle =('b'+'c')*2;
+	float  rectangular_area = 'b'*'c';
+	//tinh chu vi, dien tich hinh vuong 
+	float square_perimeter = 'a' + 'a' + 'a' + 'a'; 
+	float area_square = 'a'*'a';
+	
+ 
+		
+	 
+	 
+	 
+
+} 
